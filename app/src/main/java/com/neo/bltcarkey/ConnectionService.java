6e40a582384e5dddf5b6412e9a479088abd8674c
neoxia <xia19890119@163.com>
2020-05-07 22:15:21 +0800

    Initial commit

---
+++ app/src/main/java/com/neo/bltcarkey/ConnectionService.java
@@ -0,0 +1,15 @@
+package com.neo.bltcarkey;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class ConnectionService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
